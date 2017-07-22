d932595855e197d71333f6e5d1a49e8ae9ae8802
Earl Sabal <earlsabal@gmail.com>
2017-07-21 23:08:45 -0700

    Update twilio controller with play and fizzbuzz methods, import TwiMLException

--- app/controllers/TwilioController.java
+++ app/controllers/TwilioController.java
@@ -1,6 +1,16 @@
 package controllers;
 import play.mvc.*;
 
+import com.twilio.twiml.TwiMLException;
+
 public class TwilioController extends Controller {
+
+	public Result play() throws TwiMLException{
+		return ok();
+	}
+
+	public Result fizzBuzz() {
+		return ok();
+	}
 
 }
