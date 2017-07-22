35f12773733b079bd0ce971a82ac581af59aa3b5
Earl Sabal <earlsabal@gmail.com>
2017-07-21 22:52:49 -0700

    Add Twilio sdk and compiled project

--- build.sbt
+++ build.sbt
@@ -8,3 +8,6 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.12.2"
 
 libraryDependencies += guice
+
+// https://mvnrepository.com/artifact/com.twilio.sdk/twilio-java-sdk
+libraryDependencies += "com.twilio.sdk" % "twilio-java-sdk" % "7.0.0-rc-10"
---
+++ target/scala-2.12/routes/main/controllers/routes.java
@@ -0,0 +1,21 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:C:/Users/Earl/Desktop/projects/PhoneBuzz/conf/routes
+// @DATE:Fri Jul 21 22:51:54 PDT 2017
+
+package controllers;
+
+import router.RoutesPrefix;
+
+public class routes {
+  
+  public static final controllers.ReverseHomeController HomeController = new controllers.ReverseHomeController(RoutesPrefix.byNamePrefix());
+  public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets(RoutesPrefix.byNamePrefix());
+
+  public static class javascript {
+    
+    public static final controllers.javascript.ReverseHomeController HomeController = new controllers.javascript.ReverseHomeController(RoutesPrefix.byNamePrefix());
+    public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets(RoutesPrefix.byNamePrefix());
+  }
+
+}
