9f28426e35eb5562041ed824404be81dd8de8a91
Earl Sabal <earlsabal@gmail.com>
2017-07-21 23:26:15 -0700

    Add content to play method, greets the player and gives instructions and returns game results, fizzbuzz not yet implemented

--- app/controllers/TwilioController.java
+++ app/controllers/TwilioController.java
@@ -1,12 +1,21 @@
 package controllers;
 import play.mvc.*;
 
+import com.twilio.twiml.Gather;
+import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.Say;
 import com.twilio.twiml.TwiMLException;
 
 public class TwilioController extends Controller {
 
 	public Result play() throws TwiMLException{
-		return ok();
+		Say message = new Say.Builder("Hello player, input a number then press pound to play PhoneBuzz")
+			.voice(Say.Voice.WOMAN).build();
+
+		Gather playRound = new Gather.Builder().action("/fizzbuzz").say(message).build();
+		VoiceResponse response = new VoiceResponse.Builder().gather(playRound).build();
+		
+		return ok(response.toXml()).as("text/xml");
 	}
 
 	public Result fizzBuzz() {
