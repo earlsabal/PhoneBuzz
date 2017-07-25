ec823b1c9480004afa1c8f7415ca3d280861c669
Earl Sabal <earlsabal@gmail.com>
2017-07-25 05:29:41 -0700

    Update index view to take in list of rounds and a message as parameters

--- target/scala-2.12/twirl/main/views/html/index.template.scala
+++ target/scala-2.12/twirl/main/views/html/index.template.scala
@@ -22,34 +22,36 @@ import play.mvc.Http.Context.Implicit._
 import play.data._
 import play.core.j.PlayFormsMagicForJava._
 
-object index extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template0[play.twirl.api.HtmlFormat.Appendable] {
+object index extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template2[List[PhoneBuzzRound],String,play.twirl.api.HtmlFormat.Appendable] {
 
   /**/
-  def apply/*1.2*/():play.twirl.api.HtmlFormat.Appendable = {
+  def apply/*1.2*/(rounds: List[PhoneBuzzRound])(message: String):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 
 
-Seq[Any](format.raw/*1.4*/("""
+Seq[Any](format.raw/*1.49*/("""
 
 """),_display_(/*3.2*/main("Welcome to Play")/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
   """),format.raw/*4.3*/("""<h1>Welcome to Play!</h1>
   <form action="/call" method="POST">
-  	Please enter a 10-digit phone number:<br>
+  	Phone number to call formatted in 10-digits:<br>
   	<input type="tel" name="phone" pattern="^\d"""),format.raw/*7.47*/("""{"""),format.raw/*7.48*/("""10"""),format.raw/*7.50*/("""}"""),format.raw/*7.51*/("""$" required><br>
   	Number of seconds delay:<br>
   	<input type="number" name="seconds"><br>
 	  <input type="submit" value="Make call">
 	</form>
-""")))}),format.raw/*12.2*/("""
+
+	<h2>"""),_display_(/*13.7*/message),format.raw/*13.14*/("""</h2>
+""")))}),format.raw/*14.2*/("""
 """))
       }
     }
   }
 
-  def render(): play.twirl.api.HtmlFormat.Appendable = apply()
+  def render(rounds:List[PhoneBuzzRound],message:String): play.twirl.api.HtmlFormat.Appendable = apply(rounds)(message)
 
-  def f:(() => play.twirl.api.HtmlFormat.Appendable) = () => apply()
+  def f:((List[PhoneBuzzRound]) => (String) => play.twirl.api.HtmlFormat.Appendable) = (rounds) => (message) => apply(rounds)(message)
 
   def ref: this.type = this
 
@@ -58,11 +60,11 @@ Seq[Any](format.raw/*1.4*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Sat Jul 22 20:28:17 PDT 2017
+                  DATE: Tue Jul 25 05:28:47 PDT 2017
                   SOURCE: C:/Users/Earl/Desktop/projects/PhoneBuzz/app/views/index.scala.html
-                  HASH: 3cb983773b42ed4fa39f7ddd43a69738bb3cd753
-                  MATRIX: 941->1|1037->3|1065->6|1096->29|1135->31|1164->34|1346->189|1374->190|1403->192|1431->193|1607->339
-                  LINES: 28->1|33->1|35->3|35->3|35->3|36->4|39->7|39->7|39->7|39->7|44->12
+                  HASH: 78454a82d522553691e1dc3d662726c2b21507d9
+                  MATRIX: 969->1|1111->48|1139->51|1170->74|1209->76|1238->79|1427->241|1455->242|1484->244|1512->245|1690->397|1718->404|1755->411
+                  LINES: 28->1|33->1|35->3|35->3|35->3|36->4|39->7|39->7|39->7|39->7|45->13|45->13|46->14
                   -- GENERATED --
               */
           
