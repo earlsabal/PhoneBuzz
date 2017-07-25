f7ad3bf34e8b570124f5b1bea48ea8a3c5841abe
Earl Sabal <earlsabal@gmail.com>
2017-07-25 03:07:24 -0700

    Remove BaseModel model

--- app/controllers/TwilioController.java
+++ app/controllers/TwilioController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
