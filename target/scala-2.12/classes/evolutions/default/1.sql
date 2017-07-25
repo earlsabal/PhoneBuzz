4fdd42f1f74a9f41ca1ad2ba7562693be22503cc
Earl Sabal <earlsabal@gmail.com>
2017-07-25 08:57:29 -0700

    Fix syntax error in evolution

--- target/scala-2.12/classes/evolutions/default/1.sql
+++ target/scala-2.12/classes/evolutions/default/1.sql
@@ -2,14 +2,13 @@
 
 # --- !Ups
 
-create table PhoneBuzzRound (
-  id                    BIGINT(20) NOT NULL AUTO_INCREMENT,
-  phoneNumber           VARCHAR(255) NOT NULL,
-  secondsDelayed				BIGINT(20) NOT NULL,
-  inputNumber						BIGINT(20) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE phonebuzzrounds (
+  id SERIAL PRIMARY KEY,
+  phoneNumber VARCHAR(255) NOT NULL,
+  secondsDelayed BIGINT NOT NULL,
+  inputNumber BIGINT NOT NULL
 );
 
 # --- !Downs
 
-DROP TABLE PhoneBuzzRound;
+DROP TABLE if exists phonebuzzrounds;
